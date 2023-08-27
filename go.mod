75d3bb566ea7dd5f7f6e2834b824896b6c63983b
Sam Barrett <barrett370@gmail.com>
2023-08-27 02:01:02 +0100

    use lib

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/barrett370/dynamic-nextdns
 
 go 1.20
 
-require github.com/barrett370/cloudflare_dynamicdns v0.0.0-20230219143920-98fb65fd18d3
+require github.com/barrett370/crongo v0.0.0-20230827005336-b7185ae43b4b
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"log"
@@ -11,10 +12,10 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/barrett370/cloudflare_dynamicdns/cron"
+	"github.com/barrett370/crongo"
 )
 
-type IPUpdateService struct {
+type ipUpdateService struct {
 	target string
 	client *http.Client
 }
@@ -25,24 +26,23 @@ const (
 	envNextDNSIntervalSeconds = "NEXTDNS_INTERVAL_SECONDS"
 )
 
-func NewIPUpdateService(target string) *IPUpdateService {
-	return &IPUpdateService{
+func newIPUpdateService(target string) *ipUpdateService {
+	return &ipUpdateService{
 		target: target,
 		client: http.DefaultClient,
 	}
 }
 
-func (i IPUpdateService) Run(logger *log.Logger) error {
+func (i ipUpdateService) Run(ctx context.Context) error {
 	resp, err := i.client.Get(i.target)
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
-	respBytes, err := io.ReadAll(resp.Body)
+	_, err = io.ReadAll(resp.Body)
 	if err != nil {
 		return err
 	}
-	logger.Print(string(respBytes))
 	return nil
 }
 
@@ -67,12 +67,20 @@ func main() {
 		}
 	}
 	log.Printf("Starting update service: profile: %s, target: %s, interval seconds: %d\n", nextDNSProfile, nextDNSTarget, nextDNSIntervalSeconds)
-	svc := NewIPUpdateService(nextDNSTarget)
-	cronSvc := cron.New(fmt.Sprintf("DynamicNextDNS [%s]", nextDNSProfile), svc, time.Duration(nextDNSIntervalSeconds)*time.Second)
+	svc := newIPUpdateService(nextDNSTarget)
+	errc := make(chan error)
+	cronSvc := crongo.New(fmt.Sprintf("DynamicNextDNS [%s]", nextDNSProfile), svc, time.Duration(nextDNSIntervalSeconds)*time.Second, crongo.WithErrorsOut(errc))
+	go func() {
+		for err := range errc {
+			log.Printf("error from cronner, %v", err)
+		}
+	}()
 	cronSvc.Start()
+
 	interruptC := make(chan os.Signal, 1)
 	signal.Notify(interruptC, os.Interrupt, syscall.SIGTERM)
 	<-interruptC
 	log.Println("received os interrupt or kill, stopping update processes..")
 	cronSvc.Stop()
+	close(errc)
 }
